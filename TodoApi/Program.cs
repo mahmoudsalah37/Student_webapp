6feb39e425257a3e29c0a84330288624ab58f48b
Mahmoud Salah Eldin <mahmoudsalah37@gmail.com>
2026-03-10 09:20:14 +0200

    Add initial project structure and configuration files for TodoApi

---
+++ TodoApi/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentEnrollment.Data;
+using StudentEnrollment.Data.Contracts;
+using StudentEnrollment.Data.Repositories;
+using TodoApi.Configurations;
+using TodoApi.Endpoints;
+
+var builder = WebApplication.CreateBuilder(args);
+var con = builder.Configuration.GetConnectionString("SchoolDbConnection");
+builder.Services.AddDbContext<StudentEnrollmentDbContext>(options =>
+    options.UseNpgsql(con).UseSnakeCaseNamingConvention());
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IStudentRepository, StudentRepository>();
+builder.Services.AddScoped<ICourseRepository, CourseRepository>();
+builder.Services.AddScoped<IEnrollmentRepository, EnrollmentRepository>();
+
+builder.Services.AddAutoMapper(typeof(MapperConfig));
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseHttpsRedirection();
+
+// Endpoints
+app.MapStudentEndpoints();
+app.MapEnrollmentEndpoints();
+app.MapCourseEndpoints();
+
+app.Run();
